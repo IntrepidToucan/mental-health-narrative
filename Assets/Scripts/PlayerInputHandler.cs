167e8e0cf69ba7ab7c316de08819e034f3636573
Intrepid Toucan <96765951+IntrepidToucan@users.noreply.github.com>
2024-06-09 21:20:19 -0700

    feat: finish implementing player movement and jumping using raycasting

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -5,13 +5,14 @@ using UnityEngine.InputSystem;
 public class PlayerInputHandler : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float moveSpeed = 4f;
     [SerializeField] private float accelerationTimeGrounded = 0.1f;
     [SerializeField] private float accelerationTimeAirborne = 0.2f;
     
     [Header("Jumping")]
-    [SerializeField] private float jumpHeight = 4f;
-    [SerializeField] private float timeToJumpApex = 0.4f;
+    [SerializeField] private float jumpHeight = 1f;
+    [SerializeField] private float distanceToJumpApex = 1.8f;
+    [SerializeField] private float fallingGravityScale = 2f;
     
     private PlayerMovementController _movementController;
     
@@ -36,7 +37,7 @@ public class PlayerInputHandler : MonoBehaviour
             _velocity.y = 0;
         }
 
-        var jumpInitialVelocity = 2 * jumpHeight / timeToJumpApex;
+        var jumpInitialVelocity =  2 * jumpHeight * moveSpeed / distanceToJumpApex;
 
         if (_jumpAction.IsPressed() && _movementController.Collisions.Below)
         {
@@ -44,7 +45,12 @@ public class PlayerInputHandler : MonoBehaviour
         }
         
         var moveValue = _moveAction.ReadValue<Vector2>();
-        var gravity = -2 * jumpHeight / Mathf.Pow(timeToJumpApex, 2);
+        var gravity = -2 * jumpHeight * Mathf.Pow(moveSpeed, 2) / Mathf.Pow(distanceToJumpApex, 2);
+
+        if (_velocity.y <= Mathf.Epsilon)
+        {
+            gravity *= fallingGravityScale;
+        }
 
         _velocity.x = Mathf.SmoothDamp(_velocity.x, moveValue.x * moveSpeed, ref _velocityXSmoothing,
             _movementController.Collisions.Below ? accelerationTimeGrounded : accelerationTimeAirborne);
