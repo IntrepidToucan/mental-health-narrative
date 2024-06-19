5889f421b941ae9a73fca638bb1d9f5ee155b811
Cam <137089034+stiver32@users.noreply.github.com>
2024-06-19 13:57:14 -0700

    Work on Inventory system
    Add functions to itemUI prefab and corresponding script

--- Assets/Scripts/Scripts_UI/InventoryItem.cs
+++ Assets/Scripts/Scripts_UI/InventoryItem.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.EventSystems;
 
 public class InventoryItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TMP_Text quantityTxt;
+    [SerializeField] private Image borderImage;
+
+    public event Action<InventoryItem> OnItemClicked, 
+        OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag, OnRightMouseBtnClick;
+
+    private bool empty = true;
+
+    private void Awake()
     {
-        
+        ResetData();
+        Deselect();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetData()
     {
-        
+        itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+
+    public void Deselect()
+    {
+        borderImage.enabled = false;
+    }
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        itemImage.gameObject.SetActive(true);
+        itemImage.sprite = sprite;
+        quantityTxt.text = quantity.ToString();
+        empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+
+    public void OnBeginDrag()
+    {
+        if (empty)
+            return;
+
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnDrop()
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnEndDrag()
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnPointerClick(BaseEventData data)
+    {
+       PointerEventData pointerData = (PointerEventData)data;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
     }
 }
