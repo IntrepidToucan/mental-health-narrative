382be0dffba19f1664663917f592aa37efbcda17
Cam <137089034+stiver32@users.noreply.github.com>
2024-08-16 11:59:17 -0700

    Work on Erol Level

--- Assets/Scripts/Prototype/ExplosionTrigger.cs
+++ Assets/Scripts/Prototype/ExplosionTrigger.cs
@@ -1,17 +1,63 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplosionTrigger : MonoBehaviour
 {
-    [Header("Explosion Settings")]
-    [SerializeField] private PlatformExplosion platformExplosion; // Reference to the PlatformExplosion script
-    [SerializeField] private string triggeringTag = "Box"; // The tag of the object that can trigger the explosion (e.g., "Box")
+    [Header("Item Requirements")]
+    [SerializeField] private bool requiresItems = false;  // Toggle to enable/disable item requirements
+    [SerializeField] private List<Item> requiredItems;    // List of required items (ScriptableObjects)
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private bool areItemsPlaced = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Check if the triggering object is the one we want (e.g., a box or other object)
-        if (other.CompareTag(triggeringTag))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            platformExplosion.TriggerExplosion();
+            if (requiresItems && !areItemsPlaced)
+            {
+                if (CheckForRequiredItems())
+                {
+                    areItemsPlaced = true;
+                    // Remove items from inventory
+                    RemoveRequiredItems();
+                    Debug.Log("All required items placed. The device is now armed.");
+                }
+                else
+                {
+                    Debug.Log("Required items are missing. The device cannot be armed.");
+                    return;
+                }
+            }
+
+            // Proceed to trigger the explosion if items are placed or no items are required
+            TriggerExplosion();
         }
+    }
+
+    private bool CheckForRequiredItems()
+    {
+        foreach (var item in requiredItems)
+        {
+            if (!Inventory.Instance.HasItem(item.itemId))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void RemoveRequiredItems()
+    {
+        foreach (var item in requiredItems)
+        {
+            Inventory.Instance.RemoveItem(item.itemName);
+        }
+    }
+
+    private void TriggerExplosion()
+    {
+        // Logic to trigger the explosion
+        Debug.Log("Explosion triggered!");
+        // Add your explosion logic here
     }
 }
